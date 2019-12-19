e3cabfd03c968ac7c7d328466bb0aa3bfb4a521c
Masatoshi Tsushima <utisam@gmail.com>
2019-12-19 23:39:24 +0900

    Use react-virtualized

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,43 +1,70 @@
 import moment from 'moment';
 import React, { useEffect, useState } from 'react';
+import { AutoSizer, List, ListRowProps } from 'react-virtualized';
+import 'react-virtualized/styles.css';
 import './App.css';
-
 
 interface LogEntry {
   timestamp: moment.Moment,
   text: string,
 }
 
-const eventSource = new EventSource('/events/log');
+interface LogEntryRowProps {
+  entry: LogEntry,
+  style: React.CSSProperties,
+}
 
-const LogList: React.FC = () => {
+const LogEntryRow: React.FC<LogEntryRowProps> = (props) => {
+  const entry = props.entry;
+  return (<div className="LogEntryRow" style={props.style}>
+    <div className="LogEntryRow__Timestamp">{entry.timestamp.format('HH:mm:ss.SSSSSS')}</div>
+    <div>{entry.text}</div>
+  </div>)
+}
+
+
+const LogEntryList: React.FC<{logs: LogEntry[]}> = (props) => {
+  return (<AutoSizer>
+      {({width, height}) =>
+        <List
+        height={height}
+        rowHeight={32}
+        rowRenderer={({index, key, style}: ListRowProps) => (
+          <LogEntryRow key={key} entry={props.logs[index]} style={style} />
+          )}
+          rowCount={props.logs.length}
+          scrollToIndex={props.logs.length - 1}
+          width={width}
+          />
+        }
+    </AutoSizer>);
+}
+
+const App: React.FC = () => {
+  // const logs = [
+  //   { timestamp: moment(), text: "aaa" },
+  //   { timestamp: moment(), text: "aaa" },
+  //   { timestamp: moment(), text: "aaa" },
+  //   { timestamp: moment(), text: "aaa" },
+  //   { timestamp: moment(), text: "aaa" },
+  // ];
+
   const [logs, setLogs] = useState<LogEntry[]>([]);
   useEffect(() => {
+    const eventSource = new EventSource('/events/log');
     eventSource.addEventListener('message', (event) => {
       const entry = JSON.parse(event.data);
 
-      setLogs((logs) => [...logs, {
+      setLogs((logs: LogEntry[]) => [...logs, {
         timestamp: moment(entry['timestamp']),
         text: entry['text'] || '',
       }]);
     });
   }, []);
-  useEffect(() => {
-    const lastEntries = document.getElementsByClassName('last-entry');
-    if (lastEntries.length > 0) {
-      lastEntries[0].scrollIntoView();
-    }
-  }, [logs]);
 
-  return (<ul>
-    {logs.map((entry, idx) => <li key={idx} className={(idx == logs.length - 1) ? "last-entry" :""}>[{entry.timestamp.format('HH:MM:SS')}] {entry.text}</li>)}
-  </ul>);
-}
-
-const App: React.FC = () => {
   return (
     <div className="App">
-      <LogList />
+      <LogEntryList logs={logs} />
     </div>
   );
 }
